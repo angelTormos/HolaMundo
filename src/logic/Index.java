a559b71d880604020c8f1752416f6bda7f5592b4
Angel Tormos Gual <angtorgua2@alu.edu.gva.es>
2024-02-08 13:50:18 +0100

    Commit codigo Tarea 3

---
+++ src/logic/Index.java
@@ -0,0 +1,17 @@
+package logic;
+
+/**
+ * Comentario para nuestra clase principal main
+ * @author angel
+ * @version 1.0
+ */
+public class Index {
+    /**
+     * Comentario para nuestro metodo principal main
+     * @param args
+     */
+    public static void main(String[] args) {
+        Logic objSumar = new Logic();
+        System.out.println(objSumar.sumar(2,2));
+    }
+}
---
+++ src/logic/Logic.java
@@ -0,0 +1,22 @@
+package logic;
+/**
+ * CLase para sumar dos numeros enteros,
+ * Escribe aqui comentarios sobre tu clase
+ * @version 1.0
+ * @author angel
+*/
+public class Logic {
+    /**
+     * Constructor
+     */
+    public Logic(){}
+    /**
+     * Metodo sumar de la clase <coda>logic</coda>
+     * @param a primer parametro de entrada
+     * @param b segundo parametro de entrada
+     * @return Retorno entero la suma de los enteros a y b
+     */
+    public int sumar(int a, int b){
+        return a + b;
+    }
+}
